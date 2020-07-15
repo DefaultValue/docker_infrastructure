7d99ef0fc5a76abede1b7aa587c4bbc60cb89dfb
Maksym Zaporozhets <maksimz@default-value.com>
2020-07-15 09:50:44 +0300

    Changed the `docker-rebuild.sh` file to work with `docker-compose.yml` instead of `docker-compose-dev.yml` by default (reported by HOF-company)

--- templates/project/docker-rebuild.sh
+++ templates/project/docker-rebuild.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 # >>> Turn off composition, remove containers
-docker-compose -f docker-compose-dev.yml down
+docker-compose -f docker-compose.yml down
 # >>> Remove ALL stopped containers
 # docker rm $(docker ps -a -q)
-# >>> Remove all images
+# >>> Clean Docker images cache
 # docker image prune -af # --all --force
 # >>> Remove all volumes
 # docker volume prune -f
 # >>> Run the composition
-docker-compose -f docker-compose-dev.yml up -d --force-recreate --build
-# docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --force-recreate --build
-# >>> Run the composition in the production mode (without debug and under the root user)
-# docker-compose -f docker-compose.yml  up -d --build --force-recreate
+docker-compose -f docker-compose.yml up -d --force-recreate --build
+# >>> Run composition with multiple docker-compose configuration files if needed
+# docker-compose -f docker-compose.yml -f docker-compose-override.yml up -d --force-recreate --build
 # >>> For MacOS users: start docker-sync-stack
 # docker-sync-stack start
